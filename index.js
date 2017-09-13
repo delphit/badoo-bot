eb3ab4ea5cf65ce8670767da8a4ce94843c168ab
IgVol <igor.voloshenko@keenethics.com>
2017-09-13 20:06:27 +0300

    Try added function for Get elements from page

--- index.js
+++ index.js
@@ -1,43 +1,75 @@
-const Nightmare = require('nightmare');
-const _ = require('lodash');
-const co = require('co');
+const Nightmare = require("nightmare");
+const _ = require("lodash");
+const co = require("co");
 const nightmare = Nightmare({ show: true });
-const dotenv = require('dotenv').config({ silent: true });
-const { email, password, girlCity } = process.env;
+const dotenv = require("dotenv").config({ silent: true });
+const { email, password, location, score } = process.env;
+
+const selectors = [
+  { key: "location", selector: ".js-location-label" },
+  { key: "score", selector: ".js-profile-score" },
+  { key: "interests", selector: ".js-interests-board" }
+];
+
+const getUserInfo = function(selector) {
+  return nightmare
+    .exists(selector)
+    .then(element => {
+      if (element) {
+        nightmare.evaluate(function(selector) {
+          return document.querySelector(selector).innerText;
+        }, selector);
+      }
+      return false;
+    })
+    .catch(e => console.log("fffffffffffffffffffffff", e));
+};
 
 const like = function*() {
   yield nightmare.wait(500);
-  yield nightmare.click('.js-profile-header-toggle-layout');
-  yield nightmare.wait('.js-location-label');
+  yield nightmare.click(".js-profile-header-toggle-layout");
+  yield nightmare.wait("body");
+  const userInfo = yield selectors.map(url => {
+    return {
+      [url.key]: getUserInfo(url.selector)
+    };
+  });
   yield nightmare
-    .evaluate(function() {
-      return {
-        location: document.querySelector('.js-location-label').innerText,
-      };
-    })
-    .then(function({ location }) {
-      console.log('\t Location:   ', location);
-      if (location === girlCity) {
-        console.log('yes -> click');
-        return nightmare.type('body', '1');
+    .evaluate(() => {
+      console.log(
+        "\t Location:",
+        userInfo, // тут ніхера немає...
+        "Score",
+        userInfo.score,
+        "Interests",
+        userInfo.interests
+      );
+      if (
+        userInfo || // тут ніхера немає...
+        userInfo.location === location ||
+        userInfo.score < score
+      ) {
+        console.log("yes -> click");
+        return nightmare.type("body", "1");
       } else {
-        console.log('no -> click');
-        return nightmare.type('body', '2');
+        console.log("no -> click");
+        return nightmare.type("body", "2");
       }
     })
-    .catch(err => console.log('Error in the like function =>>>', err));
+    .catch(e => console.log(e));
+  console.log("llloooggg", userInfo);
 };
 
 const auth = function*() {
-  yield nightmare.goto('https://badoo.com/ru/signin/?f=top');
-  yield nightmare.wait('.js-signin-password');
+  yield nightmare.goto("https://badoo.com/ru/signin/?f=top");
+  yield nightmare.wait(".js-signin-password");
   yield nightmare
-    .type('.js-signin-login', email)
-    .type('.js-signin-password', password)
-    .click('.sign-form__submit');
+    .type(".js-signin-login", email)
+    .type(".js-signin-password", password)
+    .click(".sign-form__submit");
   yield nightmare
-    .wait('.js-profile-layout-container')
-    .catch(err => console.log('Error in the login function =>>>', err));
+    .wait(".js-profile-layout-container")
+    .catch(e => console.log(e));
 };
 
 co(function*() {
@@ -45,4 +77,4 @@ co(function*() {
   for (let i = 0; i < 1000; i++) {
     yield like();
   }
-}).then(() => console.log('finished!'), e => console.log(e));
+}).then(() => console.log("finished!"), e => console.log(e));
